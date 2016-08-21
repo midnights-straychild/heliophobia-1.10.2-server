ae54404c5e07be828d12f3aa48f123e9984c69e6
Ralf Lauseker <midnights.straychild@gmail.com>
2016-08-21 15:24:47 +0200

    added configs and start scripts

---
+++ ServerStart.bat
@@ -0,0 +1,1 @@
+java -server -Xmn1G -Xms4096m -Xmx4096M -XX:PermSize=256m -d64 -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+UseCodeCacheFlushing -XX:MaxGCPauseMillis=250 -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar forge-1.10.2-12.18.1.2070-universal.jar nogui pause
