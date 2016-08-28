d607e0d258a0984afc55ffec23bd9395695afc12
Ralf Lauseker <midnights.straychild@gmail.com>
2016-08-28 23:12:40 +0200

    added a few mods, config changes.

--- ServerStart.sh
+++ ServerStart.sh
@@ -1,7 +1,3 @@
 cd "$(dirname "$0")"
-if [ ! -f minecraft_server.1.7.10.jar ]; then
-   echo "running install script!"
-   ./FTBInstall.sh
-fi
  echo "make sure to read eula.txt before playing!"
  java -server -Xmn1G -Xms4096m -Xmx4096M -XX:PermSize=256m -d64 -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+UseCodeCacheFlushing -XX:MaxGCPauseMillis=250 -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar forge-1.10.2-12.18.1.2070-universal.jar nogui
